383a67cb24f69785f6608a485aec83470fb14be4
GinoK9 <220242915+CatDamUniba@users.noreply.github.com>
2025-07-24 18:55:32 +0200

    Delete SAOS2025 directory

--- SAOS2025/src/main/java/CatDam/SAOS2025/models/RuoloEnum.java
+++
@@ -1,7 +0,0 @@
-package CatDam.SAOS2025.models;
-
-public enum RuoloEnum {
-    UTENTE,
-    MODERATORE,
-    ADMIN,
-}
--- SAOS2025/src/main/java/CatDam/SAOS2025/repositories/LibroRepository.java
+++
@@ -1,8 +0,0 @@
-package CatDam.SAOS2025.repositories;
-
-import CatDam.SAOS2025.entities.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibroRepository extends JpaRepository<Libro, String> {}
