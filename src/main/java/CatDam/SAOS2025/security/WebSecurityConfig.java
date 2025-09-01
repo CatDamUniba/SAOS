5af58483e2ad170a06ce836859ca8ef87bc40c2b
GinoK9 <220242915+CatDamUniba@users.noreply.github.com>
2025-09-01 19:33:41 +0200

    Add files via upload

--- src/main/java/CatDam/SAOS2025/security/WebSecurityConfig.java
+++ src/main/java/CatDam/SAOS2025/security/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:5500","http://127.0.0.1:5500"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
