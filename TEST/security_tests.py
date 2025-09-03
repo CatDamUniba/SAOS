d1fbf17946ca9305eee8a232bc91b456788a755a
GinoK9 <220242915+CatDamUniba@users.noreply.github.com>
2025-09-03 19:28:30 +0200

    Add files via upload

--- TEST/security_tests.py
+++ TEST/security_tests.py
@@ -1,15 +1,16 @@
 import requests
 import jwt
 import datetime
+import warnings
 
 # Configurazione base
 BASE_URL = "http://127.0.0.1:8080"
 LOGIN_ENDPOINT = f"{BASE_URL}/api/auth/login"
 BOOKS_ENDPOINT = f"{BASE_URL}/api/libro/findAllBooks"
 
-USERNAME = "admin"      # Cambia con un utente admin reale
-PASSWORD = "admin123"   # Cambia con la password reale
-SECRET_KEY = "sRSecretKey"  # Deve corrispondere alla tua jwtSecret se usi HS512
+USERNAME = "test_user"      # Cambia con un utente admin reale
+PASSWORD = "gfg123"         # Cambia con la password reale
+SECRET_KEY = "aldoaldoaldoaldoaldoaldoaldoaldoaldoaldo"  # Deve corrispondere alla tua jwtSecret se usi HS512
 
 # Funzione login per ottenere un JWT valido
 def get_valid_jwt():
@@ -18,7 +19,7 @@ def get_valid_jwt():
         data = res.json()
         return data["accessToken"]
     else:
-        print("❌ Login fallito:", res.status_code, res.text)
+        print("KO Login fallito:", res.status_code, res.text)
         return None
 
 # Funzione per testare chiamata con un token arbitrario
@@ -28,11 +29,14 @@ def test_token(token, description):
     res = requests.get(BOOKS_ENDPOINT, headers=headers)
     print(f"Status code: {res.status_code}")
     if res.status_code == 200:
-        print("✅ Accesso consentito")
+        print("OK Accesso consentito")
     else:
-        print("❌ Accesso negato:", res.text[:200])
+        print("KO Accesso negato:", res.text[:200])
 
 def main():
+    # ignora tutte le warning presenti di tipo deprecation per rendere più pulito l'esito dei test
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    
     # 1. Otteniamo un token valido
     valid_token = get_valid_jwt()
     if not valid_token:
@@ -54,9 +58,7 @@ def main():
     test_token(expired_token, "Token scaduto")
 
     # 3. Token con algoritmo 'none' (NON firmato)
-    none_token = jwt.encode(
-        {"sub": USERNAME}, key=None, algorithm=None
-    )
+    none_token = "eyJhbGciOiJub25lIn0.eyJzdWIiOiAidGVzdFVzZXIifQ."
     test_token(none_token, "Token con alg=none (non firmato)")
 
     # 4. Token con claim falsificato (issuer sbagliato)
